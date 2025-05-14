7a0a75a3df6ef156cec92ef0df4b0e35a0737f9c
NeoKitsune <codingalice@gmail.com>
2025-05-13 20:59:37 -0400

    Part1 of DXT5 decoding

--- cmd/arcEdit/main.go
+++ cmd/arcEdit/main.go
@@ -10,44 +10,62 @@ var xor = []byte{
 	0xea, 0x57, 0xbd, 0xef}
 
 var xor2 = []byte{
-	0xea, 0xea, 0x15, 0x15,
-	0x15, 0x15, 0x15, 0x15,
-	0x1D, 0xf1, 0x3d, 0xc9,
-	0xbf, 0xbf, 0xbf, 0xbf}
+	0xea, 0x57, 0xbd, 0xef,
+	0x58, 0xb0, 0x60, 0x00,
+	0xd1, 0x9a, 0x16, 0x4c,
+	0x7b, 0xbe, 0xe7, 0xe9}
 
 func decrypt0x10(file, out []byte) error {
 	size := len(file) - 0x80
-	fmt.Printf("%x %x\n", size, size/0x10)
-	buf := []byte{}
-	for j := range 0x10 {
-		for i := range size / 0x10 {
-			// this+0x4 = inBuf ^ EA
-			buf = append(buf, file[i+j+0x80]^xor2[j%0x10])
-		}
-	}
-
-	for i, x := range buf {
-		if i%0x10 == 0 {
-			fmt.Println()
-		}
-		fmt.Printf("%.2x ", x)
-	}
-	fmt.Println()
-
+	lent := 0x1000
 	writen := 0
 	offset := 0x0
 	for size > 0 {
-		lent := 0x1000
+		// Not accruate
 		if size < 0x1000 {
 			lent = size
 		}
 		stride := (lent / 0x10)
+
+		fmt.Printf("FileSize:%x Stride:%x\n", size, stride)
+		buf := []byte{}
+		if stride&0xf == 0 {
+			fmt.Println("TBI")
+			break
+
+			// for j := range 0x10 {
+			// 	for range stride {
+			// 	}
+			// }
+		} else {
+			iz := 0
+			for j := range 0x10 {
+				for range stride {
+					// this+0x4 = inBuf ^ EA
+					index := file[(iz)+0x80]
+					key := xor2[j%0x10]
+					buf = append(buf, index^key)
+					iz++
+				}
+			}
+		}
+
+		for i, x := range buf {
+			if i%0x10 == 0 {
+				fmt.Printf("\n%.3X - ", i)
+			} else if i%4 == 0 {
+				fmt.Print("| ")
+			}
+			fmt.Printf("\033[9%dm%.2X \033[0m", (i%stride)%0x8, x)
+		}
+		fmt.Println()
+
 		for i := 0x0; i < stride; i++ {
 			// index := i * 4
 			for j := 0; j < 0x10; j++ {
 				// index := j * (stride * 2)
 				// fmt.Printf("%x\n", index)
-				out[(i+j)+offset+0x80] = buf[j*stride]
+				out[(i+j)+offset+0x80] = buf[i+(j*stride)]
 				writen++
 			}
 			offset += 15
