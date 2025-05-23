b975953e4147d3216e3fe2264a75abb79fc310ce
NeoKitsune <codingalice@gmail.com>
2025-05-23 19:53:13 -0400

    DXT1 algo

--- cmd/arcEdit/main.go
+++ cmd/arcEdit/main.go
@@ -15,6 +15,38 @@ var xor2 = []byte{
 	0xd1, 0x9a, 0x16, 0x4c,
 	0x7b, 0xbe, 0xe7, 0xe9}
 
+func decrypt0x8(file, out []byte) error {
+	size := len(file) - 0x80
+	lent := 0x1000
+	offset := 0x0
+	for size > 0 {
+		if size < 0x1000 {
+			lent = size
+		}
+		stride := (lent / 0x8)
+		buf := []byte{}
+		iz := 0
+		for j := range 0x8 {
+			for range stride {
+				index := file[(iz)+offset+0x80]
+				key := xor2[j%0x8]
+				buf = append(buf, index^key)
+				iz++
+			}
+		}
+		for i := 0x0; i < stride; i++ {
+			for j := 0; j < 0x8; j++ {
+				out[(i+j)+offset+0x80] = buf[i+(j*stride)]
+			}
+			offset += 0x7
+		}
+		offset += stride
+		size -= lent
+	}
+
+	return nil
+}
+
 func decrypt0x10(file, out []byte) error {
 	size := len(file) - 0x80
 	lent := 0x1000
@@ -103,6 +135,8 @@ func main() {
 	//if reflect.DeepEqual(file[0x54:0x57], []byte{'D', 'X', 'T', '5'}) {}
 	var err error
 	switch file[0x57] {
+	case '1':
+		err = decrypt0x8(file, out)
 	case '5':
 		err = decrypt0x10(file, out)
 	case 0x0:
